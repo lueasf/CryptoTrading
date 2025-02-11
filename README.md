8b62eec4c5bc24327a212d7b6e78180165c3bb14
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2025-02-11 13:18:47 +0100

    end of small proj

--- README.md
+++ README.md
@@ -20,9 +20,30 @@ les "sliding windows".
 
 Moving Average Crossover : C'est une stratégie de trading qui consiste à acheter ou vendre un actif lorsque deux moyennes mobiles se croisent.
 
+# Backtesting
+Le backtesting est une méthode qui permet de tester une stratégie ou un modèle sur des données historiques. 
+Ici, j'effectue le backtesting avec un portefeuille de 10 000$.
+On test avec la stratégie Buy and Hold et la stratégie SMA. 
+Au bout de 1 an, B&H atteint 40 000$ et SMA atteint 45 000$ .
+On quadruple notre investissement. C'est parce que en 2020, le Bitcoin a explosé.
+Si on recommence avec les données de 2021, on se rend compte, que
+B&H surpasse Crossing Averages à la fin et les rendements sont moins élevés qu'en 2020.
+
 ## Yfinance
 - download() est une méthode de yfinance utilisé pour récupérer les données d'une action.
 
 ## Pandas
 - head() est une méthode de pandas utilisé pour afficher les premières lignes d'un dataframe (tableau de données).
 - rolling() est une méthode de pandas utilisé pour calculer les MA.
+- diff() est une méthode de pandas utilisé pour calculer la différence entre deux lignes.
+- cumsum() est une méthode de pandas utilisé pour calculer la somme cumulée.
+- cumprod() est une méthode de pandas utilisé pour calculer le produit cumulé.
+
+# Matlplotlib
+- plot() est une méthode de matplotlib utilisé pour afficher un graphique.
+```python
+ax2.plot(trade_sig.loc[trade_sig['Position'] == 1.0].index, trade_sig.Short[trade_sig['Position'] == 1.0],
+marker=6, ms=4, linestyle='none', color='red')
+```
+Cette ligne permet de placer un marker rouge sur le graphique à chaque fois que la position est à 1.0, indiquant un signal d'achat.
+
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.dates import DateFormatter
 
-BTC_USD = yf.download("BTC-USD", start="2020-01-01", end="2020-12-31", interval="1d")
+BTC_USD = yf.download("BTC-USD", start="2021-01-01", end="2021-12-31", interval="1d")
 BTC_USD['SMA_9'] = BTC_USD['Close'].rolling(window=9, min_periods=1).mean() # On ajoute une colone au dataframe, avec les MA.
 # print(BTC_USD.tail(7))
 
@@ -28,12 +28,13 @@ ax.legend(fontsize=5)
 
 ### Moving Average Crossover Stratégie
 trade_sig = pd.DataFrame(index=BTC_USD.index) # On crée un dataframe vide avec les dates de BTC_USD
-short_window = 9
-long_window = 30
+short_window = 10
+long_window = 50
 trade_sig['Short'] = BTC_USD['Close'].rolling(window=short_window, min_periods=1).mean()
 trade_sig['Long'] = BTC_USD['Close'].rolling(window=long_window, min_periods=1).mean()
 
 trade_sig['Signal'] = 0.0 # On crée une colonne de signaux vide
+trade_sig['Signal'] = np.where(trade_sig['Short'] > trade_sig['Long'], 1.0, 0.0)
 
 '''
 Stratégie:
@@ -46,6 +47,63 @@ dans la colonne Signal.
 court intervalle.
 
 Ensuite, si le signal est 1, on achète, si le signal est 0, on vend.
+Pour cela, on regarde la différence entre deux lignes et si elle vaut 1 ou -1, une ligne
+a croisé l'autre. Si c'est un 0, c'est que les deux lignes ne se sont pas croisées.
 '''
 
+trade_sig['Position'] = trade_sig['Signal'].diff()
 # print(trade_sig)
+
+fig2, ax2 = plt.subplots(dpi=500)
+date_format = DateFormatter("%h %d 20%y")
+ax2.xaxis.set_major_formatter(date_format)
+
+ax2.tick_params(axis='x', labelsize=5)
+ax2.tick_params(axis='y', labelsize=5)
+
+fig2.autofmt_xdate()
+
+ax2.plot(BTC_USD['Close'], lw=0.5, label='Closing Price') # lw = line width
+ax2.plot(trade_sig['Short'], lw=0.5, label='Short Term MA')
+ax2.plot(trade_sig['Long'], lw=0.5, label='Long Term MA')
+
+# flèches indiquants les meilleures positions d'achat et de vente
+ax2.plot(trade_sig.loc[trade_sig['Position'] == 1.0].index, trade_sig.Short[trade_sig['Position'] == 1.0],
+marker=6, ms=4, linestyle='none', color='red')
+ax2.plot(trade_sig.loc[trade_sig['Position'] == -1.0].index, trade_sig.Short[trade_sig['Position'] == -1.0],
+marker=6, ms=4, linestyle='none', color='green')
+
+ax2.set_title("Bitcoin to USD Exchange Rate", fontsize=10)
+ax2.set_ylabel("Price of Bitcoin in USD", fontsize=5)
+ax2.grid()
+ax2.legend(fontsize=5)
+plt.close(fig)
+# plt.show()
+
+# backtesting
+initial_capital = 10000.0 # USD
+backtest = pd.DataFrame(index=BTC_USD.index)
+backtest['BTC_Return'] = BTC_USD['Close'] / BTC_USD['Close'].shift(1) # Auj / Hier
+
+# Si le signal est 1, on achète, sinon on ne fait rien
+backtest['Alg_Return'] = np.where(trade_sig.Signal == 1, backtest.BTC_Return, 1.0)
+backtest['Balance'] = initial_capital * backtest.Alg_Return.cumprod()
+
+fig3, ax3 = plt.subplots(dpi=500)
+date_format = DateFormatter("%h %d 20%y")
+ax3.xaxis.set_major_formatter(date_format)
+ax3.tick_params(axis='x', labelsize=5)
+ax3.tick_params(axis='y', labelsize=5)
+fig3.autofmt_xdate()
+
+# Buy and Hold : On ne touche plus à rien
+ax3.plot(initial_capital * backtest.BTC_Return.cumprod(), lw=0.5, label='Buy and Hold')
+
+# Crossing Averages
+ax3.plot(backtest['Balance'], lw=0.5, label='Crossing Averages')
+
+ax3.set_title('Value of Portfolio')
+ax3.set_ylabel('USD')
+ax3.grid()
+ax3.legend()
+plt.show()
